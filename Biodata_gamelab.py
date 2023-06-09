46cbbaf749095cb1d2c7f4b1b1e85fc7adefdc08
Marsha06 <123293130+Marsha06@users.noreply.github.com>
2023-06-09 10:44:25 +0800

    Add files via upload

---
+++ Biodata_gamelab.py
@@ -0,0 +1,20 @@
+print ("=======================================")
+print ("Program Biodata Sederhana Dengan Python")
+print ("=======================================")
+ 
+nama = "Ni Kadek Marsha Listyani"
+hobi = "seorang kpopers"
+alamat = "Subamia Jln Sekawan Rajawali No10A"
+Kota = "Tabanan"
+Propinsi = "Bali"
+
+print("Nama Lengkap  :",nama)
+
+print("Alamat        :",alamat, "\n                Kota ", Kota, "Propinsi ",Propinsi)
+
+
+print("Hobi          :", hobi)
+
+print ("Selamat Datang di",nama )
+print ("=======================================")
+ 
